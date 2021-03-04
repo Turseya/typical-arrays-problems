3f3e0eaaf0a1414985ce4fe93d5689db00a73d93
Turseya <turseya@gmail.com>
2021-03-04 22:05:47 +0300

    init: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+
+  if (typeof array == 'undefined' ||array.length === 0) {
+    return 0;
+  };
+
+  const minArray = array.sort((a,b) => a - b);
+  
+  return minArray[0];
+};
 
 exports.max = function max (array) {
-  return 0;
+
+  if (typeof array == 'undefined' || array.length === 0) {
+    return 0;
+  };
+
+  const maxArray = array.sort ((a,b) => b - a);
+  return maxArray[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if (typeof array == 'undefined' ||array.length === 0) {
+    return 0;
+  };
+ const result = array.reduce((sum,current) => sum + current, 0);
+  return result / array.length;
 }
